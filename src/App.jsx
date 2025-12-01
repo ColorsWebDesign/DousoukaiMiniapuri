1c7b5597c9caaeecd1ecf4dae506b6d849720dc7
ColorsWebDesign <fujiwara@recap.jp>
2025-12-01 09:42:20 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,12 +27,12 @@ function App() {
 
       <div class="nav">
         <div class="clm03">
-          <div>
-            <img src="https://github.com/ColorsWebDesign/DousoukaiMiniapuri/blob/17b1e983da96cb9d28659384be0f2bf538efff6b/img/nav001.jpg" alt="">
-          </div>
-          <div><p>プロフィール登録</p></div>
-          <div><p>同窓会</p></div>
-          <div><p>お問い合わせ</p></div>
+          <img src="https://github.com/ColorsWebDesign/DousoukaiMiniapuri/blob/17b1e983da96cb9d28659384be0f2bf538efff6b/img/nav001.jpg" alt="">
+          <img src="https://github.com/ColorsWebDesign/DousoukaiMiniapuri/blob/17b1e983da96cb9d28659384be0f2bf538efff6b/img/nav001.jpg" alt="">
+          <img src="https://github.com/ColorsWebDesign/DousoukaiMiniapuri/blob/17b1e983da96cb9d28659384be0f2bf538efff6b/img/nav001.jpg" alt="">
+          <img src="https://github.com/ColorsWebDesign/DousoukaiMiniapuri/blob/17b1e983da96cb9d28659384be0f2bf538efff6b/img/nav001.jpg" alt="">
+          <img src="https://github.com/ColorsWebDesign/DousoukaiMiniapuri/blob/17b1e983da96cb9d28659384be0f2bf538efff6b/img/nav001.jpg" alt="">
+          <img src="https://github.com/ColorsWebDesign/DousoukaiMiniapuri/blob/17b1e983da96cb9d28659384be0f2bf538efff6b/img/nav001.jpg" alt="">
         </div>
       </div>
     </div>
